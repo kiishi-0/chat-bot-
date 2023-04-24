675b75864719752e03cd2b6b6bed83c6796787f1
Iteoluwakiishi.Oyewo <kiishioluwa@gmail.com>
2023-04-25 00:00:59 +0100

    Solved the issue with the model and train data

--- nltk_utils.py
+++ nltk_utils.py
@@ -14,7 +14,7 @@ def stem(word):
 
 def  bag_of_words(tokenized_sentence, all_words):
     tokenized_sentence = [stem(w) for w in tokenized_sentence]
-    print(tokenized_sentence)
+    # print(tokenized_sentence)
     bag = np.zeros(len(all_words), dtype = np.float32)
 
     for idx, w in enumerate(all_words):
--- train.py
+++ train.py
@@ -1,10 +1,11 @@
 import json
 from nltk_utils import tokenize, stem, bag_of_words
 import numpy as np
-
 import torch
-import torch.nn
+import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
+from model import NeuralNet
+import train
 
 with open('intents.json', 'r') as f:
     intents = json.load(f)
@@ -27,18 +28,67 @@ all_words = [stem(w) for w in all_words if w not in ignore_words]
 all_words = sorted(set(all_words))
 tags = sorted(set(tags))
 
-
 X_train = []
 Y_train = []
 for(pattern_sentence, tag) in xy:
     bag = bag_of_words(pattern_sentence, all_words)
-
     X_train.append(bag)
-
     label = tags.index(tag)
-
     Y_train.append(label)
 
 X_train = np.array(X_train)
-Y_train = np.array(Y_train)
+Y_train = torch.from_numpy(np.array(Y_train)).long()
 
+class ChatDataset(Dataset):
+    def __init__(self):
+        self.n_samples = len(X_train)
+        self.x_data = X_train
+        self.y_data = Y_train
+
+    def __getitem__(self, index):
+        return self.x_data[index], self.y_data[index]
+
+    def __len__(self):
+        return self.n_samples
+
+if __name__ == '__main__':
+    # Hyperparameters
+    print('dinj vfdknvfkj')
+    batch_size = 12
+    hidden_size =12
+    output_size = len(tags)
+    input_size = len(X_train[0])
+    learning_rate = 0.001
+    num_epochs = 1000
+
+    dataset = ChatDataset()
+    train_loader = DataLoader(dataset = dataset, batch_size=batch_size, shuffle=True, num_workers=2)
+    print('dinj vfdknvfkj')
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model = NeuralNet(input_size, hidden_size, output_size).to(device)
+
+    # loss and optimizer
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr = learning_rate)
+
+    loss = None
+    outputs = None
+    print('dinj vfdknvfkj')
+    for epoch in range(num_epochs):
+        for (words, labels) in train_loader:
+            words = words.to(device)
+            labels = labels.to(device)
+
+            #forward
+            outputs = model(words)
+            loss = criterion(outputs, labels)
+
+            # backward and optimizer step
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+        if(epoch + 1) % 100 == 0:
+            print(f'epoch {epoch + 1}/{num_epochs}, loss={loss.item():.4f}')
+
+    print(f'final loss, loss={loss.item():.4f}')
